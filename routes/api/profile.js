58a386f452409869f8057f1aedaf2d288206061e
金勇男 <kin@kinisaonoMacBook-puro.local>
2020-07-03 08:09:11 +0900

    profile part 1

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const auth = require('../../middleware/auth')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 const router = express.Router()
 
-// @route  GET api/profile
-// @desc   Test route
-// @access Public
-router.get('/', (req, res) => res.send('Profile Route'))
+// @route  GET api/profile/me
+// @desc   get profile by user id
+// @access Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar'])
+        if (!profile) {
+            return res.status(400).json({ msg: 'No profile.' })
+        }
+        return res.json(profile)
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).send('Server Error.')
+    }
+})
 
 module.exports = router
